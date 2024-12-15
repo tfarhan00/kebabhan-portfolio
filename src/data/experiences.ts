43ba19edab6f531f533653a1f179c964f1a715a4
hpep40 <elisaweibert@outlook.com>
2024-12-15 17:14:56 +0700

    add embelishment

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,7 +1,7 @@
 export const experiences = [
   {
     year: "2024",
-    title: "Software Engineer",
+    title: "Web3 Software Engineer",
     company: "Engowl Studio",
     date: "January 2024 - January 2025",
   },
@@ -13,13 +13,13 @@ export const experiences = [
   },
   {
     year: "2023",
-    title: "Frontend Developer",
+    title: "Front-end Developer",
     company: "Netra",
     date: "March 2023 - September 2023",
   },
   {
     year: "2022",
-    title: "Frontend Developer (Internship)",
+    title: "Front-end Developer (Internship)",
     company: "Barrans Global",
     date: "October 2022 - March 2023",
   },
